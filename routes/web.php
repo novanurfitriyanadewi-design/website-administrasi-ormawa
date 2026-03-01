ef72498c1ea7724a8b9bb46142cd561f5e60354e
nova <novanurfitriyanadewi@saintek.unipdu.ac.id>
2026-03-01 10:19:05 +0700

    feat: inisiasi project Laravel – [Nova Nurfitriyana]

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/perkenalan', function () {
+
+    return '<h1>Halo! Nama saya Nova Nurfitriyana Dewi</h1>
+
+            <p>NIM: 4124051 | Prodi: Sistem Informasi</p>
+
+            <p>Saya siap belajar Laravel! 🚀</p>';
+
+});

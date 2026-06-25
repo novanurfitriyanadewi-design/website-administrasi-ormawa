e5d05833172f51ef024d2a49073e6f37ac1d70fb
novanurfitriyanadewi-design <novanurfitriyanadewi@saintek.unipdu.ac.id>
2026-06-25 20:52:53 +0700

    419

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        // 🔥 DISABLE CSRF KHUSUS LOGIN (UNTUK TEST 419)
+        $middleware->validateCsrfTokens(except: [
+            '/login',
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
